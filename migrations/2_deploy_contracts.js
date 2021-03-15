6ff1062fdb6ab5efbe9689945edab7dd58df779c
Kat Leight <leightkt@gmail.com>
2021-03-14 18:27:06 -0600

    updating ethereum.request language

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
-const ToDoList = artifacts.require("./TodoList.sol");
+const ConvertLib = artifacts.require("ConvertLib");
+const MetaCoin = artifacts.require("MetaCoin");
 
 module.exports = function(deployer) {
-    deployer.deploy(ToDoList);
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
 };
--- src/app.js
+++ src/app.js
@@ -1,51 +1,16 @@
-App = {
-    load: async () => {
-        await loadAccout()
-    }
-}
+const $ethbutton = document.querySelector('.enableEthereumButton')
 
-loadWeb3: async () => {
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider
-      web3 = new Web3(web3.currentProvider)
-    } else {
-      window.alert("Please connect to Metamask.")
-    }
-    // Modern dapp browsers...
-    if (window.ethereum) {
-      window.web3 = new Web3(ethereum)
-      try {
-        // Request account access if needed
-        await ethereum.enable()
-        // Acccounts now exposed
-        web3.eth.sendTransaction({/* ... */})
-      } catch (error) {
-        // User denied account access...
-      }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      App.web3Provider = web3.currentProvider
-      window.web3 = new Web3(web3.currentProvider)
-      // Acccounts always exposed
-      web3.eth.sendTransaction({/* ... */})
-    }
-    // Non-dapp browsers...
-    else {
-      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
+if (typeof window.ethereum !== 'undefined') {
+    console.log('MetaMask is installed!');
   }
 
-loadAccount: async () => {
-      App.account = await ethereum.request({ method: 'eth_accounts'})
-}
 
-loadContract: async () => {
-    const todoList = await $.getJSON('TodoList.json')
-}
-
-$(() => {
-    $(window).load(() => {
-        App.load()
-    })
+$ethbutton.addEventListener('click', () => {
+    getAccount()
 })
+
+async function getAccount() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+    const account = accounts[0]
+    console.log(account)
+}
